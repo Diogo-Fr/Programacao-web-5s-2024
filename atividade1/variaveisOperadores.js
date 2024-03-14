09201992eff3b2c5ca9bf45dc1863bf7de8a45d2
Diogo <diogofransasantos@gmail.com>
2024-03-14 10:24:30 -0300

    adicionar atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,27 @@
+//Soma
+let as = 5
+let bs = 10
+
+let cs = as + bs;
+console.log('O resultado da soma entre 5 e 10 é ' + cs)
+
+//Subtração
+let ab = 10
+let bb = 5
+
+let cb = ab - bb;
+console.log('O resultado da Subtração entre 10 e 5 é ' + cb)
+
+//Multiplicação
+let am = 30
+let bm = 5
+
+let cm = am * bm;
+console.log('O resultado da Multiplicação entre 30 e 5 é ' + cm)
+
+//Divisão
+let ad = 100
+let bd = 10
+
+let cd = ad / bd;
+console.log('O resultado da Divisão entre 100 e 10 é ' + cd)
